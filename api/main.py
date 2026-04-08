ca0ebfbb807c50601084f7e01aa7260a3143ad03
Anirudh Gowri Sankaran <121012958+anirudh5-creator@users.noreply.github.com>
2026-04-08 19:08:29 +0530

    Delete api directory

--- api/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from rag.query_engine import query_system
-
-app = FastAPI()
-
-@app.get("/ask")
-def ask(query: str):
-    answer = query_system(query)
-    return {"answer": answer}
